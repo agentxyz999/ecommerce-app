691450dc8b5b229a81e6eb61da85d5e95879cff4
Rolie Conde <onefivezero999@gmail.com>
2023-03-08 02:52:04 +0800

    started the top bar and made it responsive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import Hero from "./components/Hero";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      {/* <Navigation />
+      <Hero /> */}
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/TemporaryDrawer.jsx
@@ -0,0 +1,88 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MenuOpenIcon from "@mui/icons-material/MenuOpen";
+import CancelIcon from "@mui/icons-material/Cancel";
+import PeopleIcon from "@mui/icons-material/People";
+import CheckroomIcon from "@mui/icons-material/Checkroom";
+
+export default function TemporaryDrawer() {
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <CancelIcon
+        sx={{ padding: "20px", color: "#d0312d", fontSize: "40px" }}
+      />
+      <List>
+        {["All Categories", "Men", "Women", "Kids"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <PeopleIcon />
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All brands", "Nike", "Addidas"].map((text, index) => (
+          <ListItem key={index} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <CheckroomIcon />
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      <React.Fragment>
+        <MenuOpenIcon onClick={toggleDrawer("right", true)} color="primary" />
+
+        <Drawer
+          anchor={"right"}
+          open={state["right"]}
+          onClose={toggleDrawer("right", false)}
+        >
+          {list("right")}
+        </Drawer>
+      </React.Fragment>
+    </div>
+  );
+}
